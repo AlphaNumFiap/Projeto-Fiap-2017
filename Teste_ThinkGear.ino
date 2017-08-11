d09a9295d723ee8333b7035f1c2b716192d80aa2
AlphaNumFiap <30758255+AlphaNumFiap@users.noreply.github.com>
2017-08-11 00:48:36 -0300

    Add files via upload

---
+++ Teste_ThinkGear.ino
@@ -0,0 +1,17 @@
+#include "thinkgear.h"
+
+char *comPortName = NULL;
+    int   dllVersion = 0;
+    int   connectionId = 0;
+    int   packetsRead = 0;
+    int   errCode = 0;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  dllVersion = TG_GetDriverVersion();
+  connectionId = TG_GetNewConnectionId();
+}
